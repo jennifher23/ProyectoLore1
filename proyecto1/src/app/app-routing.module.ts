efcb481125c012d972b3d8d7c93801ffb4f0e35d
jennifher23 <jpb.chugchilan@yavirac.edu.ec>
2022-11-23 21:15:22 -0500

    Tarea n2 Rutas principales y rutas hijas

--- proyecto1/src/app/app-routing.module.ts
+++ proyecto1/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
+//import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProductComponent } from './pages/product/product.component';
 
-const routes: Routes = [];
+ const routes:Routes = [
+    //rutas protegidas
+    {path:'',
+    component:PagesComponent,
+    children:[
+    //pages
+    {path:'dashboard', component:DashboardComponent},
+    {path:'catalogue', component:CatalogueComponent},
+    {path:'products', component:ProductComponent},
+    //{path:'', redirectTo:'/dashboard', pathMatch:'full'},
+    ]},
+    //auth
+    {path:'login', component:LoginComponent},
+    {path:'register', component:RegisterComponent},
 
+    
+    {path:'**', component:NotFoundComponent}    
+ ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    //CommonModule
+  ],
+  exports:[RouterModule],
 })
 export class AppRoutingModule { }
--- proyecto1/src/app/app.module.ts
+++ proyecto1/src/app/app.module.ts
@@ -1,41 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { UserComponent } from './user/user/user.component';
+import { UserComponent } from './pages/user/user.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { HeaderComponent } from './static/header/header.component';
-import { NavComponent } from './static/nav/nav.component';
 import { FooterComponent } from './static/footer/footer.component';
+import { SidebarComponent } from './static/sidebar/sidebar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { LoginComponent } from './auth/login/login.component';
 import { BreadcrumbsComponent } from './static/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './static/sidebar/sidebar.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidebarComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent,
     UserComponent,
     RegisterComponent,
     HeaderComponent,
-    NavComponent,
-    FooterComponent,
+    FooterComponent,    
     DashboardComponent,
-    NotfoundComponent,
+    NotFoundComponent,
     LoginComponent,
     BreadcrumbsComponent,
-    SidebarComponent,
-   
+    PagesComponent,
+    ProductComponent,
+    CatalogueComponent        
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
